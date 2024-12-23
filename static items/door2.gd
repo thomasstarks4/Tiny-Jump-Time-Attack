2ed6848c0a0052b0560808c712284144ec2d6163
thomasstarks4 <thomas.a.starks4@gmail.com>
2024-12-22 22:23:09 -0500

    Main Menu
    Created basic main menu component to start and quit the game.

--- static items/door2.gd
+++ static items/door2.gd
@@ -1,26 +1,23 @@
 extends Node2D
 
-var isOpen = false
+var is_open: bool = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Ensure the door starts in the 'closed' state
 	$DoorSprite.play('close')
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
-
-
 func _on_area_2d_body_entered(body):
-	if body.name == 'Player':
-		if !isOpen:
-			$DoorSprite.play('open')
-			isOpen = true
-			
+	# Open the door when the player enters the area
+	if body.name == "Player" and not is_open:
+		$DoorSprite.play('open')
+		is_open = true
 
 
 func _on_door_entered_area_body_entered(body):
-	if body.name == 'Player':
-		get_tree().change_scene_to_file("res://levels/level_3.tscn")
+	if body.name == "Player":
+		call_deferred("change_scene", "res://levels/level_3.tscn")
+
+func change_scene(scene_path: String):
+	get_tree().change_scene_to_file(scene_path)
+
